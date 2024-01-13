74c2951289044cca2399ebb91419e318d7f56070
Li4nee <nishantpokharel9@gmail.com>
2024-01-13 15:39:03 +0545

    setting up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import app from './app.js';
 dotenv.config();
 
-connectDB();
+connectDB()
+.then(()=>
+{   
+ app.on("error",(err)=>{
+        throw err;
+    })
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server Running At Port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log(`Server setup failed !!!! ${process.env.PORT}`);
+})
